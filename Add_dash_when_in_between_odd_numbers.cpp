e502210b4a6ed79dfa66811e2a478415e7ebde79
Ajmal Hadi <89326831+oblivious97@users.noreply.github.com>
2024-02-17 21:39:22 +0800

    Add files via upload

---
+++ Add_dash_when_in_between_odd_numbers.cpp
@@ -0,0 +1,30 @@
+
+//question 12
+
+#include <iostream>
+using namespace std;
+
+string dashNumber(string numbers){
+    string store = "";
+    for(int x = 0; x < numbers.length(); x++){
+        if((numbers[x] == '1' || numbers[x] == '3' || numbers[x] == '5' 
+        || numbers[x] == '7' || numbers[x] == '9') && (numbers[x+1] == '1' || numbers[x+1] == '3' || numbers[x+1] == '5' 
+        || numbers[x+1] == '7' || numbers[x+1] == '9')){
+            store += numbers[x];
+            store += int(45); //in ASCII
+            }
+        else{
+           store += numbers[x]; 
+        }
+        }
+        return store;
+    }
+    
+
+
+int main()
+{
+    cout << dashNumber("213421429324") << endl;
+
+    return 0;
+}
